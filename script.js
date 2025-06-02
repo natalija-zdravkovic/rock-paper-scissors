5bf78346cb6d9811d9e677cbafbfdfe09d2fd4f3
natalija-zdravkovic <201510014+natalija-zdravkovic@users.noreply.github.com>
2025-06-02 12:22:34 +0200

    Add getComputerChoice function
    Added function that returns random "rock", "paper", or "scissors"

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Hello World!") 
+/*funkcija getComputerChoice koja vraca choice
+let number bira random broj izmedju 0, 1 i 2
+ukoliko je number == 0, onda je choice rock,
+ukoliko je number == 1, onda je choice paper,
+ukoliko je number == 2, onda je choice scissors*/
+
+function getComputerChoice()
+{
+    let number = Math.floor(Math.random() * 3)
+    
+    if (number == 0)
+    {
+        return "rock"
+    }
+
+    else if (number == 1)
+    {
+        return "paper"
+    }
+
+    else
+    {
+        return "scissors"
+    }
+
+}
+
+let choice = getComputerChoice()
+console.log(choice)
