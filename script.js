9fc83529d8245c6a265742a6a0ff4eca328582e1
natalija-zdravkovic <201510014+natalija-zdravkovic@users.noreply.github.com>
2025-06-03 12:57:47 +0200

    Add getHumanChoice function
    Added function to get user input using prompt

--- script.js
+++ script.js
@@ -7,23 +7,38 @@ ukoliko je number == 2, onda je choice scissors*/
 function getComputerChoice()
 {
     let number = Math.floor(Math.random() * 3)
-    
+    let choice
+
     if (number == 0)
     {
-        return "rock"
+        choice = "rock"
     }
 
     else if (number == 1)
     {
-        return "paper"
+        choice = "paper"
     }
 
     else
     {
-        return "scissors"
+        choice = "scissors"
     }
 
+    return choice
 }
 
-let choice = getComputerChoice()
-console.log(choice)
+let computerChoice = getComputerChoice()
+console.log(computerChoice)
+
+/*funckija getHumanChoice koja uzima izbor korisnika i vraca ga
+koiristi prompt metod
+zanemariti slucaj ukoliko korisnik ne unese jedan od tri izbora*/
+
+function getHumanCHoice()
+{
+    let choice = prompt("Please enter rock, paper or scissors: ")
+    return choice
+}
+
+let humanChoice = getHumanCHoice()
+console.log(humanChoice)
