fc018e71c103de61ef865fe1856997ef927964c9
natalija-zdravkovic <201510014+natalija-zdravkovic@users.noreply.github.com>
2025-07-14 02:35:00 +0200

    Add functionality for choice buttons Added functionality for choice buttons and implemented score tracking for each round

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-/*funkcija getComputerChoice koja vraca choice
-let number bira random broj izmedju 0, 1 i 2
-ukoliko je number == 0, onda je choice rock,
-ukoliko je number == 1, onda je choice paper,
-ukoliko je number == 2, onda je choice scissors*/
-
 function getComputerChoice()
 {
     let number = Math.floor(Math.random() * 3)
@@ -27,116 +21,113 @@ function getComputerChoice()
     return choice
 }
 
-/*funckija getHumanChoice koja uzima izbor korisnika i vraca ga
-koiristi prompt metod
-zanemariti slucaj ukoliko korisnik ne unese jedan od tri izbora*/
-
-function getHumanCHoice()
-{
-    let choice = prompt("Please enter rock, paper or scissors: ")
-    return choice
-}
-
-/*promenljive koje cuvaju score korisnika i kompjutera*/
-
-let humanScore = 0
-let computerScore = 0
-
-/*funkcija playRound koja kao argumente uzima izbor korisnika i kompjutera,
-nakon odigrane runde, povecava score pobednika i stampa ko je koga pobedio (papir je pobedio kamen i sl.)
-humanChoice parametar je case-insensitive*/
-
-/*korisnik i kompjuter
-papir i papir -> nereseno *
-papir i kamem -> korisnik *
-papir i makaze -> kompjuter *
-kamen i papir -> kompjuter *
-kamen i kamen -> nereseno *
-kamen i makaze -> korisnik *
-makaze i papir -> korisnik *
-makaze i makaze -> nereseno *
-makaze i kamen -> kompjuter *
-*/
+let humanScore = 0;
+let computerScore = 0;
 
 function playRound(humanChoice, computerChoice)
 {
     if (humanChoice.toLowerCase() == computerChoice)
     {
-        console.log("It's a draw!")
+        return "It's a draw!"
     }
 
     else if (humanChoice.toLowerCase() == "paper" && computerChoice == "rock")
     {
-        console.log("You win! Paper beats Rock")
         humanScore += 1
+        return "You win! Paper beats Rock"
     }
 
     else if (humanChoice.toLowerCase() == "paper" && computerChoice == "scissors")
     {
-        console.log("You lose! Scissors beats Paper")
         computerScore += 1
+        return "You lose! Scissors beats Paper"
     }
 
     else if (humanChoice.toLowerCase() == "rock" && computerChoice == "paper")
     {
-        console.log("You lose! Paper beats Rock")
         computerScore += 1
+        return "You lose! Paper beats Rock"
     }
 
     else if (humanChoice.toLowerCase() == "rock" && computerChoice == "scissors")
     {
-        console.log("You win! Rock beats Scissors")
         humanScore += 1
+        return "You win! Rock beats Scissors"
     }
 
     else if (humanChoice.toLowerCase() == "scissors" && computerChoice == "paper")
     {
-        console.log("You win! Scissors beats Paper")
         humanScore += 1
+        return "You win! Scissors beats Paper"
     }
 
     else if (humanChoice.toLowerCase() == "scissors" && computerChoice == "rock")
     {
-        console.log("You lose! Rock beats Scissors")
         computerScore += 1
+        return "You lose! Rock beats Scissors"
     }
 }
-
-/*funkcija playGame pet puta ponavlja rundu i na kraju stampa rezultat korisnika i kompjutera
-i ko je pobedio */
-
-function playGame()
-{
-    //for (let i = 0; i < 5; i++)
-    while (true)
-    {
-        const computerSelection = getComputerChoice()
-        const humanSelection = getHumanCHoice()
-
-        playRound(humanSelection, computerSelection)
-    }
-
-    console.log("Your score is: " + humanScore)
-    console.log("Computer's score is: " + computerScore)
-    
-    if (humanScore > computerScore)
-    {
-        console.log("You win! Congratulations!")
-    }
-
-    else if (computerScore > humanScore)
-    {
-        console.log("You lose!")
-    }
-
-    else
-    {
-        console.log("It's draw!")
-    }
-}
-
-//playGame()
 
 const playButton = document.querySelector(".play.button")
+let computer = document.querySelector(".computer .points")
+let message = document.querySelector("#game")
+let human = document.querySelector(".user .points")
 
-playButton.addEventListener("click", playGame)
+playButton.addEventListener("click", () => {
+    message.textContent = "Choose paper, rock or scissors";
+    humanScore = 0;
+    computerScore = 0;
+    computer.textContent = computerScore;
+    human.textContent = humanScore;
+    buttons.forEach(button => {
+        button.addEventListener("click", play)
+    })
+})
+
+const paperButton = document.querySelector(".paper.button")
+const rockButton = document.querySelector(".rock.button")
+const scissorsButton = document.querySelector(".scissors.button")
+
+let humanSelection;
+let computerSelection;
+
+paperButton.addEventListener("click", () => {
+    humanSelection = "paper"
+})
+
+rockButton.addEventListener("click", () => {
+    humanSelection = "rock"
+})
+
+scissorsButton.addEventListener("click", () => {
+    humanSelection = "scissors"
+})
+
+const buttons = document.querySelectorAll(".choice .button")
+
+function play() 
+{
+    computerSelection = getComputerChoice();
+    message.textContent = playRound(humanSelection, computerSelection);
+    computer.textContent = computerScore;
+    human.textContent = humanScore;
+
+    if (humanScore == 5 || computerScore == 5)
+    {
+        buttons.forEach(button => {
+            button.removeEventListener("click", play)
+        })
+    }
+
+    if (humanScore == 5)
+    {
+        message.textContent = "You won the game!"
+    }
+
+    else if (computerScore == 5)
+    {
+        message.textContent = "Computer won the game!"
+    }
+}
+
+
